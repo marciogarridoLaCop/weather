5399d3621d41375c598c0355b96c92ee237ab510
faajalbani <faajalbani@gmail.com>
2024-02-08 14:17:04 -0300

    Saneamento
    Saneamento

--- weather/myproject/api/urls.py
+++ weather/myproject/api/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('evapotranspiracao', views.getEvapotranpiracaoCSV),
-    path('dadosevapotranspiracao', views.getEvapotranpiracao),
-    path('dadosevapotranspiracaodb', views.getEvapotranpiracaoDB),
     path('evapotranspiracaoservice', views.getEvapotranspiracaoService),
 ]
     
